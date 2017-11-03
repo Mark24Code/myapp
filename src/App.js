b37762d5db821056af2cd33564e365f2c44d86b7
Mark24 <mark.zhangyoung@qq.com>
2017-11-03 18:04:00 +0800

    添加clock使用state 替代 props

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import  {List,Todo} from './List.js';
+import Clock from './Clock.js';
 
 class App extends Component {
   render() {
@@ -16,6 +17,7 @@ class App extends Component {
         </p>
         <List name="AABBA"></List>
         <Todo tname="9999999"></Todo>
+        <Clock></Clock>
       </div>
     );
   }
