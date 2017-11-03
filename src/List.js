773b10d097a8963f3f6e99414db83b9ca3cab4ea
Mark24 <mark.zhangyoung@qq.com>
2017-11-03 17:40:47 +0800

    嵌套,组件封装

---
+++ src/List.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function List(props){
+  return (<h1>Heelo, {props.name}</h1>);
+}
+
+function Todo(props){
+  return (<h2> TTTTTTOOOOOODOOOO {props.tname}</h2>);
+}
+export {List,Todo}
