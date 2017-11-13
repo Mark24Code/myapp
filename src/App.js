2ec6292d8fe995fe89e7ee8089a1d855cdb4c4b7
Mark24 <mark.zhangyoung@qq.com>
2017-11-13 13:58:37 +0800

    filterProductTable

--- src/App.js
+++ src/App.js
@@ -8,31 +8,43 @@ import UnitComp from './myState.js';
 import NameForm from './NameForm.js';
 import Reservation from './Reservation.js';
 import Inheritance from './Inheritance.js';
+import FilterableProductTable from './FilterableProductTable.js';
 
 class App extends Component {
+  constructor(props){
+      super(props);
+      this.hide = true;
+  }
+
   render() {
+    let others = "";
+    if(!this.hide){
+      others =  (          <div>
+              <div>==========================</div>
+             <Inheritance left={<h2>This is LEft</h2>} right={<h4>rrrrright</h4>} >
+                <p>888888</p>
+              </Inheritance>
+              <div>==========================</div>
+              <NameForm></NameForm>
+              <UnitComp></UnitComp>
+              <Calculator></Calculator>
+              <Clock></Clock>
+              <Reservation></Reservation>
+              <List name="AABBA"></List>
+              <p className="App-intro">
+                To get started, edit <code>src/App.js</code> and save to reload.
+              </p>
+          </div>);
+    }
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <Inheritance
-          left={<h2>This is LEft</h2>}
-          right={<h4>rrrrright</h4>}
-        >
-          <p>888888</p>
-        </Inheritance>
-        <div>==========================</div>
-        <NameForm></NameForm>
-        <UnitComp></UnitComp>
-        <Calculator></Calculator>
-        <Clock></Clock>
-        <Reservation></Reservation>
-        <List name="AABBA"></List>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <FilterableProductTable></FilterableProductTable>
+        {others}
+
       </div>
     );
   }
